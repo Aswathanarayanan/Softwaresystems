675bef2c899b8b304eeb46eeca46c97bb62db3a4
Aswathanarayanan <aswathavasan@gmail.com>
2022-09-23 09:58:52 +0530

    Update 7.c

--- handson2/7.c
+++ handson2/7.c
@@ -1,6 +1,6 @@
 /* Name: Aswatha Narayanan
  * Roll no: MT2022026
- * 
+ *
  * 7. Write a simple program to print the created thread ids.
  */
 
@@ -9,20 +9,14 @@
 #include<unistd.h>
 #include<string.h>
 void fun(void* args){
-	printf("thread id:%ld\n",pthread_self());
+        printf("thread id:%ld\n",pthread_self());
 }
 int main(){
-	void* val[3];
-	pthread_t mythread[3];
-	int thread_num[3];
-	for(int i=0;i<3;i++){
-		pthread_t mythread[i];//,mythread2,mythread3;
-		thread_num[i]=i;
-		pthread_create(&mythread[i],NULL,(void*)fun,&thread_num[i]);
-		//pthread_create(&mythread2,NULL,(void*)fun,NULL);
-		//pthread_create(&mythread3,NULL,(void*)fun,NULL);
-		//void* val;
-		pthread_join(mythread[i],&val[i]);
-	}
-	return 0;
+        void* val[3];
+        pthread_t mythread;
+        for(int i=1;i<=3;i++){
+                pthread_create(&mythread,NULL,(void*)fun,NULL);
+                //pthread_join(mythread,&val[i]);
+        }
+        return 0;
 }
