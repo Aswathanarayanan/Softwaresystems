856836e9ba0d17326c7b9777e7a720faf06ee2c0
Aswathanarayanan <aswathavasan@gmail.com>
2022-10-01 00:37:37 +0530

    Update 8f.c

--- handson2/8f.c
+++ handson2/8f.c
@@ -2,7 +2,7 @@
 //Roll no: MT2022026
 	
 //8. Write a separate program using signal system call to catch the following signals.
-//SIGVTALRM (use setitimer system call)
+//SIGPROF (use setitimer system call)
 
 
 #include <signal.h>
@@ -15,13 +15,19 @@ void fun(){
 	exit(0);    ///to exit the function or it will be in infinte loop
 }
 int main(){
-	int singal_status = signal(SIGVTALRM,(void *)fun);
+	int signal_status = signal(SIGVTALRM,(void *)fun);
+	if(signal_status==-1)
+		perror("");
 	struct itimerval timer;
 	timer.it_value.tv_sec = 1;
     	timer.it_value.tv_usec = 0;
-        timer.it_interval.tv_sec = 1;
-    	timer.it_interval.tv_usec = 0;
+        timer.it_interval.tv_sec = 0;
+    	timer.it_interval.tv_usec = 1;
         int timer_status = setitimer(ITIMER_VIRTUAL, &timer, 0);
-	pause();
+	if(timer_status==-1)
+		perror("");
+	//int s=10,n=0;
+	//s=s/n;
+	while(1);
 	return 0;
 }
